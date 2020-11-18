07d9f84f47d32b1b691a565175550d3cdf043619
Aseem001 <aseemanand2@gmail.com>
2020-11-18 23:51:26 +0530

    [Aseem] solved PinUC4

--- PinValidator.js
+++ PinValidator.js
@@ -8,7 +8,9 @@ function validatePin(pin)
     //UC2
     //let pinRegExp = RegExp('^[0-9]{1}[a-zA-Z0-9]{5}$');
     //UC3
-    let pinRegExp = RegExp('^[0-9]{1}[a-zA-Z0-9]{4}[0-9]{1}$');
+    //let pinRegExp = RegExp('^[0-9]{1}[a-zA-Z0-9]{4}[0-9]{1}$');
+    //UC4
+    let pinRegExp = RegExp('^[0-9]{1}[a-zA-Z0-9]{2}[ ]*[a-zA-Z0-9]{2}[0-9]{1}$');
     if (pinRegExp.test(pin))
         console.log("Valid pin")
     else
